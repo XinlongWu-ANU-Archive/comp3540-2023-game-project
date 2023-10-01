378ace6bb0617896193414d78133a7b398b03814
Zhangheng Xu <u6818456@anu.edu.au>
2023-10-02 01:26:33 +1100

    let bird enemy be smarter & always face to player

--- group project/Assets/Scripts/enemies/EnemyBird.cs
+++ group project/Assets/Scripts/enemies/EnemyBird.cs
@@ -4,15 +4,86 @@ using UnityEngine;
 
 public class EnemyBird : Enemy
 {
+    private Transform player;
+    public float lineOfSite;
+    public Transform startPoint;
+    public LayerMask obstacleLayer; // Layer for obstacles
+    public float obstacleDetectionDistance = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-      base.Start();  
+        base.Start();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-      base.Update();  
+        base.Update();
+
+        if (player == null)
+        {
+            return;
+        }
+
+        if (IsPlayerInLineOfSight())
+        {
+            Chase();
+        }
+        else
+        {
+            ReturnStartPoint();
+        }
+
+        Flip();
+    }
+
+    private bool IsPlayerInLineOfSight()
+    {
+        float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
+
+        // Check if the player is within the line of sight and there are no obstacles in the way
+        if (distanceFromPlayer < lineOfSite)
+        {
+            // Cast a ray to check for obstacles between the bird and the player
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, player.position - transform.position, distanceFromPlayer, obstacleLayer);
+            if (hit.collider == null)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void ReturnStartPoint()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime);
+    }
+
+    private void Flip()
+    {
+        if (transform.position.x > player.transform.position.x)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+
+    private void Chase()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lineOfSite);
+
+        
     }
 }
