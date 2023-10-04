c3ce756dea812d08535ec560bf44ca0a00937652
Xuan Liu <u6393399@anu.edu.au>
2023-10-04 21:58:15 +1100

    UI Hearts and score added

--- group project/Assets/Scripts/GameManager.cs
+++ group project/Assets/Scripts/GameManager.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Text scoreText;
     public int score;
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
+        scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
@@ -20,6 +23,7 @@ public class GameManager : MonoBehaviour
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 
     
